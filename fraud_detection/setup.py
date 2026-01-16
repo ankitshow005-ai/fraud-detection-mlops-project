0e467067ab3695fbe3567cd403734f15aeddff26
Ankit Show <ankitshow005@gmail.com>
2026-01-16 12:10:09 +0530

    Initial project structure and experiment notebook added

---
+++ fraud_detection/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End-to-end MLOps project for financial fraud detection',
+    author='Ankit Show',
+    license='MIT',
+)
